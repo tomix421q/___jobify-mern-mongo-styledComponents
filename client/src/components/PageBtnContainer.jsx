2f80a733920d36147a4d4a71df400ec23bdd3db7
tomix421q <zilka.tomas421@gmail.com>
2024-10-09 23:54:40 +0200

    FIX StyledComponents

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,5 +1,5 @@
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
-import Wrapper from '../../assets/wrappers/pageBtnContainer'
+import Wrapper from '../assets/wrappers/PageBtnContainer'
 import { useLocation, Link, useNavigate } from 'react-router-dom'
 import { useAllJobsContext } from '../../pages/AllJobs'
 
